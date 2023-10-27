c6740906858a7eccf8b9cb43834123444ef0a2a4
Frank712 <franksuriel_7@hotmail.com>
2023-10-27 00:22:23 -0500

    ADD http server v1

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  res.write("Hello world!");
+  res.end();
+});
+
+server.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
