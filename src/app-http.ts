73bcfa55de33ae02a97ab78ec84f175746e2cd65
Frank712 <franksuriel_7@hotmail.com>
2023-10-28 20:04:43 -0500

    ADD responses styles and resource by path

---
+++ src/app-http.ts
@@ -0,0 +1,26 @@
+import fs from "fs";
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  if (req.url === "/") {
+    const htmlFile = fs.readFileSync("./public/index.html", "utf8");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(htmlFile);
+  } else if (req.url === "/css/styles.css") {
+    const stylesFile = fs.readFileSync("./public/css/styles.css", "utf8");
+    res.writeHead(200, { "Content-Type": "text/css" });
+    res.end(stylesFile);
+  } else if (req.url === "/js/app.js") {
+    const stylesFile = fs.readFileSync("./public/js/app.js", "utf8");
+    res.writeHead(200, { "Content-Type": "application/javascript" });
+    res.end(stylesFile);
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end();
+  }
+});
+
+server.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
